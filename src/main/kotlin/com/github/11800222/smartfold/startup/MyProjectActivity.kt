e39b11298c9d2860cfbfb8944c0176ec1d9bf7c2
GitHub Action <action@github.com>
2025-05-14 19:02:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/11800222/smartfold/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/11800222/smartfold/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.11800222.smartfold.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
